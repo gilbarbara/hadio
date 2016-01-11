ed643ad416056c29d0ec4e79f654d5e132ecc61d
Gil Barbara <gilbarbara@gmail.com>
2016-01-10 22:04:06 -0200

    refactor Home

--- app/scripts/components/Home.jsx
+++ app/scripts/components/Home.jsx
@@ -40,6 +40,14 @@ class Home extends React.Component {
 		}
 	}
 
+	@autobind
+	onClickExample(e) {
+		e.preventDefault();
+		const el = e.currentTarget;
+
+		document.getElementById('apiUrl').value = el.dataset.url;
+	}
+
 	render() {
 		let error;
 
@@ -54,13 +62,14 @@ class Home extends React.Component {
 				<form onSubmit={this.onSubmitForm}>
 					<fieldset className="form-group">
 						<label labelFor="apiUrl">
-							<h2>API URL</h2>
-							<p>Enter you Airtime radio api below.<br />You have to enable the "Public Airtime API" in you radio's settings
+							<p>Enter you Airtime's API URL below.<br />
+								<small>You have to enable the "Public Airtime API" in you radio's settings</small>
 							</p>
 						</label>
 						<input
 							type="text" className="form-control" id="apiUrl"
 							placeholder="https://radio.airtime.pro/api/" required />
+						<a href="#" onClick={this.onClickExample} data-url="https://phr.airtime.pro/api/">Example: https://phr.airtime.pro/api/</a>
 					</fieldset>
 					{error}
 					<button type="submit" className="btn btn-primary"><i className="i-sign-in" /> Play</button>
